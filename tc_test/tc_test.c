b64d6b9cd7e83afb19776815df25aa1f6d2a0abc
ashen <sheninxing@126.com>
2019-11-05 02:17:13 -0500

    deleted:    skynet/skynet 	deleted:    tc_test/a

--- tc_test/tc_test.c
+++ tc_test/tc_test.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h> 
+#include <fcntl.h> 
 #include <sys/types.h>
 
 #define INTCMP(i1, i2)              (((unsigned long long )i1 > (unsigned long long)i2) ?  1 : (((unsigned long long)i1 < (unsigned long long)i2 ) ? -1 : 0))
@@ -11,8 +12,57 @@ int f(int* p)
     return 0;
 }
 
-int main()
+static int* f1()
 {
+    static int j = 2;
+    return &j;
+}
+
+int  main()
+{
+    *(f1()) = 3;
+    
+
+#if 0
+    int fp = open("./aaaa", O_WRONLY | O_APPEND); 
+
+    int  ifd = 10;
+    char* pc  = "shenjinxing";
+
+    dup2(fp, ifd);
+    //ifd = dup(fp);
+    
+    //close(fp);
+    write(ifd, pc, strlen(pc));
+    close(ifd);
+#endif
+
+    return 0;
+}
+
+#if 0
+int main1()
+{
+    typedef union tagType_U
+    {
+        int i;
+        double d;
+    }Type_U;
+    
+    Type_U unUn = {1};
+    printf("union           len = [%d]\n", sizeof(Type_U));
+    printf("unionval        len = [%d]\n", sizeof(unUn));
+    
+    printf("short           len = [%d]\n", sizeof(short));
+    printf("int             len = [%d]\n", sizeof(int));
+    printf("unsigned int    len = [%d]\n", sizeof(unsigned int));
+    printf("long            len = [%d]\n", sizeof(long));
+    printf("long long       len = [%d]\n", sizeof(long long ));
+    printf("void            len = [%d]\n", sizeof(void));
+    printf("void*           len = [%d]\n", sizeof(void*));
+    printf("float           len = [%d]\n", sizeof(float));
+    printf("double          len = [%d]\n", sizeof(double));
+#if 0
     char* p = "123";
     char* p2 = p;
     int iRet = 0;
@@ -23,7 +73,6 @@ int main()
     
     printf("%d ", iRet);
     
-#if 0
     typedef union tagValue_U
     {
         int    i;
@@ -35,5 +84,4 @@ int main()
 
     return 0;
 }
-
-
+#endif
